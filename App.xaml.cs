10f3b02449769f5166ccb6765d2f4aa4e12d2d2b
ghosterenko <arsenijkonkov243@gmail.com>
2026-03-21 15:24:21 +0500

    first commit

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace prakt15_Leshukov_TRPO
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App :Application
+    {
+    }
+
+}
---
+++ Pages/SignInPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace prakt15_Leshukov_TRPO.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для SignInPage.xaml
+    /// </summary>
+    public partial class SignInPage :Page
+    {
+        public int password { get; set; }
+        public SignInPage ()
+        {
+            InitializeComponent( );
+            DataContext = this;
+        }
+
+        private void SignManager (object sender, RoutedEventArgs e)
+        {
+            if(password == 1234)
+            {
+                NavigationService.Navigate( new MainPage());
+            }
+        }
+
+        private void SignUser (object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate
+        }
+    }
+}
